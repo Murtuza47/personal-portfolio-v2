a6962e2b2d72f08195089716ad3412174a52fdbe
Ali Murtaza <50296533+Murtuza47@users.noreply.github.com>
2025-04-30 22:40:40 +0500

    chore: next config for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['personal-portfolio-v2-flax.vercel.app'],
+    unoptimized: true, // This can help if you're having issues with image optimization
+  },
+};
 
 export default nextConfig;
